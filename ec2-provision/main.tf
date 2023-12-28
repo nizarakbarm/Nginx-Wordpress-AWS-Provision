b13ff4f0e59a17df38543291c10d6c5b20410503
nizarakbarm <nizarakbarm04@gmail.com>
2023-12-28 18:34:55 +0700

    feat(terraform): Set public key for aws ec2 using terraform

--- ec2-provision/main.tf
+++ ec2-provision/main.tf
@@ -9,9 +9,15 @@ locals {
   secret_vars = yamldecode(data.aws_kms_secrets.secret_vars["secret-vars"])
 }
 
+resource "aws_key_pair" "vm_key" {
+    key_name = "vm-key"
+    public_key = TF_VAR_PUBLIC_KEY
+}
+
 resource "aws_instance" "vm_server" {
     ami = local.secret_vars.ami_id
     instance_type = "t2.micro"
+    key_name = aws_key_pair.vm_key
 
     tags = {
         App = "Wordpress"
