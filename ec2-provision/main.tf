7ac7f52ce4f04e1b6125997ba97a0ccb496dad82
nizarakbarm <nizarakbarm04@gmail.com>
2023-12-28 19:07:38 +0700

    feat(terraform): Add change port ssh in user_data of aws_instance

--- ec2-provision/main.tf
+++ ec2-provision/main.tf
@@ -18,6 +18,18 @@ resource "aws_instance" "vm_server" {
     ami = local.secret_vars.ami_id
     instance_type = "t2.micro"
     key_name = aws_key_pair.vm_key
+    user_data = <<EOF
+#!/bin/bash
+
+sed "s/^#\?Port 22$/Port 5522/g" /etc/ssh/sshd_config
+if [[ $? -ne 0 ]]; then
+    echo "Warning: change port failed!"
+    exit 1
+fi
+
+systemctl restart sshd
+exit 0
+EOF
 
     tags = {
         App = "Wordpress"
